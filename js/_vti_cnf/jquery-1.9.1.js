4dd7c80f1f920658c7f414bc734e385cb175b27f
dubansky <dubansky@iastate.edu>
2013-10-09 16:22:45 -0500

    updated index
    javascript works

---
+++ js/_vti_cnf/jquery-1.9.1.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TW|09 Oct 2013 04:54:02 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|envy-PC\\envy
+vti_modifiedby:SR|envy-PC\\envy
+vti_timecreated:TR|27 Feb 2013 23:17:58 -0000
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|09 Oct 2013 04:54:02 -0000
+vti_cacheddtm:TX|09 Oct 2013 18:19:39 -0000
+vti_filesize:IR|270493
+vti_syncwith_dubansky.com\:21:TW|09 Oct 2013 18:19:39 -0000
+vti_syncofs_dubansky.com\:21:TW|09 Oct 2013 04:54:02 -0000
